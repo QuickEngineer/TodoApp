92c1f53f49cec1014a5ab2511b8c33cca9704759
John <quick.engineer.dev@gmail.com>
2023-11-14 00:08:17 +0100

    Upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter'){
+      handleAddTask();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div className="appBox">
+        <h2 className="title">Todo App</h2>
+        
+        <div className="searchBox">
+          <input
+            type="text"
+            name="itemTextbox"
+            className="itemTextbox"
+            placeholder="Add your new todo"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button
+            type="button"
+            name="addItemBtn"
+            className="addItemBtn"
+            onClick={handleAddTask}
+          >
+            +
+          </button>
+        </div>
+
+        <div className="itemList">
+          {tasks.map((task, index) => (
+            <div className="itemContainer" key={index}>
+              <p>{task}</p>
+              <button
+                type="button"
+                className="deleteItemBtn"
+                onClick={() => {
+                  const updatedTasks = [...tasks];
+                  updatedTasks.splice(index, 1);
+                  setTasks(updatedTasks);
+                }}
+              >
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </div>
+          ))}
+
+          <div className="clearAllContainer">
+            <p>You have {tasks.length} pending tasks</p>
+            <button 
+            type="button" 
+            className="clearAllBtn"
+            onClick={() => setTasks([])}
+            >
+              Clear All
+            </button>
+          </div>
+
+        </div>
+
+    
+      </div>
+    </div>  
   );
 }
 
